102d411fb559027b0965ffcf6d85ebd56ecb8bbd
FranklinSolano <franklinsolano40@gmail.com>
2022-10-06 23:23:41 -0300

    feita mudança de tela 100% programatica em navegation e em modal

--- navegacaoExercicio/navegacaoExercicio/ViewController.swift
+++ navegacaoExercicio/navegacaoExercicio/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tela02Button: UIButton!
     
   
+    @IBOutlet weak var tela03Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,21 +24,38 @@ class ViewController: UIViewController {
     }
     
     func configElementos(){
-        tela02Button.setTitle("GoTela2", for: .normal)
+        tela02Button.setTitle("GoTela02", for: .normal)
         tela02Button.setTitleColor(.green, for: .normal)
         tela02Button.backgroundColor = .darkGray
         tela02Button.clipsToBounds = true
         tela02Button.layer.cornerRadius = 10
         tela02Button.layer.borderColor = UIColor.green.cgColor
         tela02Button.layer.borderWidth = 2
+        
+    //------
+        
+        tela03Button.setTitle("GoTela03", for: .normal)
+        tela03Button.setTitleColor(.purple, for: .normal)
+        tela03Button.backgroundColor = .darkGray
+        tela03Button.clipsToBounds = true
+        tela03Button.layer.cornerRadius = 10
+        tela03Button.layer.borderColor = UIColor.purple.cgColor
+        tela03Button.layer.borderWidth = 2
     }
 
 
     @IBAction func TappedButton(_ sender: UIButton) {
-        performSegue(withIdentifier: "goTela02", sender: nil)
+        let vc:Tela02VC? = UIStoryboard(name: "Tela02VC", bundle: nil).instantiateViewController(withIdentifier: "Tela02VC") as? Tela02VC
+        present(vc ?? UIViewController(), animated: true)
+        
     }
     
    
+    @IBAction func TappedButtonTela03(_ sender: UIButton) {
+        let vc:Tela03VC? = UIStoryboard(name: "Tela03VC", bundle: nil).instantiateViewController(withIdentifier: "Tela03VC") as? Tela03VC
+        
+        navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
+    }
     
 }
 
