0de7b537feef0d463418f0860fb41dd4d6a25ac6
FranklinSolano <franklinsolano40@gmail.com>
2022-10-06 22:40:00 -0300

    feito mudança de tela com referencia e criando outra storyboard e outra class

--- navegacaoExercicio/navegacaoExercicio/ViewController.swift
+++ navegacaoExercicio/navegacaoExercicio/ViewController.swift
@@ -9,24 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var mudarTela: UIButton!
+
     
-    func  configElementos(){
-        mudarTela.setTitle("GoTela2", for: .normal)
-        mudarTela.backgroundColor = .darkGray
-        mudarTela.clipsToBounds = true
-        mudarTela.layer.cornerRadius = 10
-        mudarTela.setTitleColor(.green, for: .normal)
-    }
+    @IBOutlet weak var tela02Button: UIButton!
+    
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       configElementos()
+        configElementos()
+        view.backgroundColor = .darkGray
+ 
+    }
+    
+    func configElementos(){
+        tela02Button.setTitle("GoTela2", for: .normal)
+        tela02Button.setTitleColor(.green, for: .normal)
+        tela02Button.backgroundColor = .darkGray
+        tela02Button.clipsToBounds = true
+        tela02Button.layer.cornerRadius = 10
+        tela02Button.layer.borderColor = UIColor.green.cgColor
+        tela02Button.layer.borderWidth = 2
     }
 
 
-    @IBAction func goTela2(_ sender: UIButton) {
-        
+    @IBAction func TappedButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "goTela02", sender: nil)
     }
+    
+   
+    
 }
 
