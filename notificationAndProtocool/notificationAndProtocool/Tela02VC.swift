2e93e184a7659752f25131392a2e9b4255047bc6
FranklinSolano <franklinsolano40@gmail.com>
2022-11-24 19:14:54 -0300

    Exercicio usando contentView e usando protocool e notification

---
+++ notificationAndProtocool/notificationAndProtocool/Tela02VC.swift
@@ -0,0 +1,55 @@
+//
+//  Tela02VC.swift
+//  notificationAndProtocool
+//
+//  Created by Franklin  Stilhano on 11/23/22.
+//
+
+import UIKit
+
+protocol Tela02VCProtocool: AnyObject {
+    func tappedMacbook()
+    func tappedImac()
+}
+
+class Tela02VC: UIViewController {
+    
+    
+    @IBOutlet weak var opcionLabel: UILabel!
+    
+    @IBOutlet weak var macbookButton: UIButton!
+    
+    @IBOutlet weak var imacButton: UIButton!
+    
+    
+    weak var delegate:Tela02VCProtocool?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configElements()
+
+        
+    }
+    
+    func configElements(){
+        view.backgroundColor = .black
+        opcionLabel.text = "Opções"
+        opcionLabel.textColor = .yellow
+    }
+    
+    
+    @IBAction func tappedMacbookButton(_ sender: UIButton) {
+//        NotificationCenter.default.post(name: .macbook, object: nil)
+        delegate?.tappedMacbook()
+        dismiss(animated: true)
+    }
+    
+    
+    @IBAction func tappedImacButton(_ sender: UIButton) {
+//        NotificationCenter.default.post(name: .imac, object: nil)
+        delegate?.tappedImac()
+        dismiss(animated: true)
+    }
+    
+}
+
